254fa5300d1f37ede4f50d93141bd9ac8989bea2
Tom-R Kvalvaag <43438127+tomrtk@users.noreply.github.com>
2021-12-24 09:11:52 +0100

    added test and handling of bad yaml formating

--- yaml_models/config.py
+++ yaml_models/config.py
@@ -5,10 +5,17 @@ import os.path
 import yaml
 
 
+class YAMLError(Exception):
+    pass
+
+
 def _load_config(path: str) -> list[tuple[str, dict[str, str | int]]]:
     if os.path.exists(path) is False:
         raise FileNotFoundError(f"config file {path} not found")
 
-    config = yaml.safe_load(open(path))
+    try:
+        config = yaml.safe_load(open(path))
+    except yaml.scanner.ScannerError as e:
+        raise YAMLError(f"error in config file, {e}")
 
     return [(val.pop("type"), val) for val in config["model"]]
